5c6af84307f817c031a47fd177996077c33ee999
eforrester <esfoures@gmail.com>
2023-01-19 14:05:17 -0500

    Updates

---
+++ precache-manifest.ecfa3fe0a93819c17b724b0c4141b18f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b2ebced39524ac746e1",
+    "url": "/GPT3-React-Website/static/css/main.5ab960f1.chunk.css"
+  },
+  {
+    "revision": "6b2ebced39524ac746e1",
+    "url": "/GPT3-React-Website/static/js/main.6b2ebced.chunk.js"
+  },
+  {
+    "revision": "4824bc4903f26c2d73f7",
+    "url": "/GPT3-React-Website/static/js/1.4824bc49.chunk.js"
+  },
+  {
+    "revision": "b5fd10eee3ab5f90d9f6",
+    "url": "/GPT3-React-Website/static/js/runtime~main.b5fd10ee.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/GPT3-React-Website/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/GPT3-React-Website/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/GPT3-React-Website/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/GPT3-React-Website/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/GPT3-React-Website/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/GPT3-React-Website/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/GPT3-React-Website/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/GPT3-React-Website/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/GPT3-React-Website/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "a4a423fe434d940b17e54ec2602908a8",
+    "url": "/GPT3-React-Website/index.html"
+  }
+];
